a9f23c40dff9ff4b1d8451d849c202b463f605a0
bigwi.hodari <bigwihodari@gmail.com>
2025-06-30 11:00:27 +0200

    server_client commit

--- server.js
+++ server.js
@@ -16,3 +16,17 @@ const service = {
     },
   },
 };
+
+	
+// http server example
+const server = http.createServer(function (request, response) {
+  response.end("404: Not Found: " + request.url);
+});
+ 
+server.listen(8000);
+ 
+// Create the SOAP server
+const xml = fs.readFileSync("productsService.wsdl", "utf8");
+soap.listen(server, "/products", service, xml, function () {
+  console.log("SOAP server running at http://localhost:8000/products?wsdl");
+});
